572a110cba81a0a1e2b4f9ce5662d1e486f0b3e4
2dam <2dam@HZ375753.TartangaLH.eus>
2021-10-25 10:45:04 +0200

    first commit

---
+++ src/classes/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Paquete que va a contener todas las clases que se van a usar en los proyectos
+ * cliente y servidor.
+ */
+package classes;
---
+++ src/exceptions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Paquete que va a contener todas las excepciones que se van a controlar en los
+ * proyectos cliente y servidor.
+ */
+package exceptions;
